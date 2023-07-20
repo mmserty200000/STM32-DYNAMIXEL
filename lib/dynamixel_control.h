51f58ad4b21c01169d2b90e7b28b5a570a73a165
mmserty200000 <82755461+mmserty200000@users.noreply.github.com>
2023-07-20 12:57:52 +0700

    Update dynamixel_control.h

--- lib/dynamixel_control.h
+++ lib/dynamixel_control.h
@@ -14,7 +14,7 @@ extern "C" {
 
 #include "stdint.h"
 #include "stm32g0xx_hal.h"
-//void dynamixel_init();
+
 uint8_t calc_checksum(uint8_t *buff, uint8_t size);
 HAL_StatusTypeDef dynamixel_send(UART_HandleTypeDef *huart, const uint8_t *send, uint8_t size_send, uint8_t *recieve, uint8_t size_recieve);
 HAL_StatusTypeDef dynamixel_read_oneb(UART_HandleTypeDef *huart, uint8_t id, uint8_t reg, uint8_t *err, uint8_t *ret_dat);
